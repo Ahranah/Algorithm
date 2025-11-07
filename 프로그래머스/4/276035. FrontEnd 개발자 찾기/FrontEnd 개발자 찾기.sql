6088f0e226f90fc88deecf45be029f35d08190d8
HyeIn Jeong <150007915+Ahranah@users.noreply.github.com>
2025-11-08 00:41:58 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
-select distinct a.ID, a.EMAIL, a.FIRST_NAME, a.LAST_NAME
+select distinct a.id, a.email, a.first_name, a.last_name
 from developers as a
-inner join skillcodes as b on (a.skill_code & b.code) != 0
-where b.category ='Front End'
-order by a.ID;
+inner join skillcodes as b on (a.skill_code & b.code != 0)
+where b.category = 'Front End'
+order by a.id;
